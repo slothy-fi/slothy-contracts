ec04ec541502ec25b396843f81f1667ca84fb202
Lorenzo Callegari 乐子睿 <115325208+zkdexter@users.noreply.github.com>
2022-10-08 21:56:41 -0500

    Some deploy scripts

---
+++ scripts/deployAaveBlocks.ts
@@ -0,0 +1,64 @@
+import { ethers } from "hardhat";
+hre = require("hardhat");
+
+async function main() {
+  // We get the contract to deploy
+  const AaveSupplyBlock = await ethers.getContractFactory("AaveSupplyBlock");
+  const aaveSupplyBlock = await AaveSupplyBlock.deploy();
+
+  await aaveSupplyBlock.deployed();
+
+  console.log("AaveSupplyBlock deployed to:", aaveSupplyBlock.address);
+  try {
+    await hre.run("verify:verify", {
+      address: aaveSupplyBlock.address,
+      constructorArguments: [],
+    });
+  } catch {
+    console.log("!!Failed to verify:", aaveSupplyBlock.address);
+  }
+  console.log("AaveSupplyBlock verified");
+
+
+  // We get the contract to deploy
+  const AaveWithdrawAllBlock = await ethers.getContractFactory("AaveWithdrawAllBlock");
+  const aaveWithdrawAllBlock = await AaveWithdrawAllBlock.deploy();
+
+  await aaveWithdrawAllBlock.deployed();
+
+  console.log("AaveWithdrawAllBlock deployed to:", aaveWithdrawAllBlock.address);
+  try {
+    await hre.run("verify:verify", {
+      address: aaveWithdrawAllBlock.address,
+      constructorArguments: [],
+    });
+  } catch {
+    console.log("!!Failed to verify:", aaveWithdrawAllBlock.address);
+  }
+  console.log("AaveWithdrawAllBlock verified");
+
+  // We get the contract to deploy
+  const AaveWithdrawPercentageBlock = await ethers.getContractFactory("AaveWithdrawPercentageBlock");
+  const aaveWithdrawPercentageBlock = await AaveWithdrawPercentageBlock.deploy();
+
+  await aaveWithdrawPercentageBlock.deployed();
+
+  console.log("AaveWithdrawPercentageBlock deployed to:", aaveWithdrawPercentageBlock.address);
+  try {
+    await hre.run("verify:verify", {
+      address: aaveWithdrawPercentageBlock.address,
+      constructorArguments: [],
+    });
+  } catch {
+    console.log("!!Failed to verify:", aaveWithdrawPercentageBlock.address);
+  }
+  console.log("AaveWithdrawPercentageBlock verified");
+
+}
+
+// We recommend this pattern to be able to use async/await everywhere
+// and properly handle errors.
+main().catch((error) => {
+  console.error(error);
+  process.exitCode = 1;
+});
---
+++ scripts/deployVault.ts
@@ -0,0 +1,29 @@
+import { ethers } from "hardhat";
+hre = require("hardhat");
+
+async function main() {
+  // We get the contract to deploy
+  const SlothyVaultFactory = await ethers.getContractFactory("SlothyVaultFactory");
+  const slothyVaultFactory = await SlothyVaultFactory.deploy();
+
+  await slothyVaultFactory.deployed();
+
+  console.log("SlothyVault deployed to:", slothyVaultFactory.address);
+  try {
+    await hre.run("verify:verify", {
+      address: slothyVaultFactory.address,
+      constructorArguments: [],
+    });
+  } catch {
+    console.log("!!Failed to verify:", slothyVaultFactory.address);
+  }
+  console.log("SlothyVault verified");
+
+}
+
+// We recommend this pattern to be able to use async/await everywhere
+// and properly handle errors.
+main().catch((error) => {
+  console.error(error);
+  process.exitCode = 1;
+});
